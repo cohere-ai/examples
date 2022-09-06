d740d328158161031c111d0020a0d7bed634851e
Meor <92068895+mrmer1@users.noreply.github.com>
2022-09-06 22:27:05 +0800

    Update article-recommender/recommender.py
    Co-authored-by: Michael <michael.kozakov@gmail.com>

--- article-recommender/recommender.py
+++ article-recommender/recommender.py
@@ -49,10 +49,22 @@ def classify_text(input_text):
 
 # Create the prompt for tags extraction
 def create_prompt(text):
-    base_prompt = "Given a news article, this program returns the list tags containing keywords of that article.\n\nArticle: japanese banking battle at an end japan s sumitomo mitsui financial has withdrawn its takeover offer for rival bank ufj holdings  enabling the latter to merge with mitsubishi tokyo.  sumitomo bosses told counterparts at ufj of its decision on friday  clearing the way for it to conclude a 3 trillion\n\nTags: sumitomo mitsui financial, ufj holdings, mitsubishi tokyo, japanese banking\n--\nArticle: france starts digital terrestrial france has become the last big european country to launch a digital terrestrial tv (dtt) service.  initially  more than a third of the population will be able to receive 14 free-to-air channels. despite the long wait for a french dtt roll-out  the new platform s bac\n\nTags: france, digital terrestrial\n--\nArticle: apple laptop is  greatest gadget  the apple powerbook 100 has been chosen as the greatest gadget of all time  by us magazine mobile pc.  the 1991 laptop was chosen because it was one of the first  lightweight  portable computers and helped define the layout of all future notebook pcs. the magazine h\n\nTags: apple, apple powerbook 100, laptop\n--\nArticle:"
+    return f"""Given a news article, this program returns the list tags containing keywords of that article.
+Article: japanese banking battle at an end japan s sumitomo mitsui financial has withdrawn its takeover offer for rival bank ufj holdings  enabling the latter to merge with mitsubishi tokyo.  sumitomo bosses told counterparts at ufj of its decision on friday  clearing the way for it to conclude a 3 trillion\n\nTags: sumitomo mitsui financial, ufj holdings, mitsubishi tokyo, japanese banking
 
-    complete_prompt = base_prompt + text + "\n\nTags:"
-    return complete_prompt
+Tags: sumitomo mitsui financial, ufj holdings, mitsubishi tokyo, japanese banking
+--
+Article: france starts digital terrestrial france has become the last big european country to launch a digital terrestrial tv (dtt) service.  initially  more than a third of the population will be able to receive 14 free-to-air channels. despite the long wait for a french dtt roll-out  the new platform s bac\n\nTags: france, digital terrestrial
+
+Tags: france, digital terrestrial
+--
+Article: apple laptop is  greatest gadget  the apple powerbook 100 has been chosen as the greatest gadget of all time  by us magazine mobile pc.  the 1991 laptop was chosen because it was one of the first  lightweight  portable computers and helped define the layout of all future notebook pcs. the magazine h
+
+Tags: apple, apple powerbook 100, laptop
+--
+Article: {text}
+
+Tags:"""
 
 # Get extractions via the Generate endpoint
 def extract_tags(complete_prompt):
